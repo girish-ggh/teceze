5abc387096d3a0af304a6bc7e2711b15d66b6bf6
Girish <girish@DESKTOP-CCG06BT>
2024-10-12 23:20:25 +0530

    initial commit

---
+++ web/themes/custom/teceze_bootstrap/js/global.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.teceze_bootstrap = {
+    attach: function (context, settings) {},
+  };
+})(Drupal);
